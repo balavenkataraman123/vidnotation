5a99abbe9f787f48d19363ec0e4fd300711ac6a2
Bala Venkataraman <balathegreat999@gmail.com>
2022-10-15 01:53:15 -0400

    2 am coding vibes

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import logging
 import os
 import re
-import sys
 import time
 import mimetypes
 from datetime import datetime
+from urllib import response
+
 from flask import Flask, Response, render_template, send_file, request
+path = "nuts"
+currenttimestamp = 0
 
 app = Flask(__name__)
 
@@ -16,11 +19,8 @@ BUFF_SIZE = 10 * MB
 
 @app.route('/')
 def home():
-    response = render_template(
-        'index.html',
-        time=str(datetime.now()),
-        video=VIDEO_PATH,
-    )
+    response = render_template('index.html', video=VIDEO_PATH, timestamp=69)
+    print('deez nuts')
     return response
 
 def partial_response(path, start, end=None):
@@ -68,14 +68,37 @@ def get_range(request):
     else:
         return 0, None
     
+@app.route('/draw', methods = ['GET','POST'])
+def index():
+    global currenttimestamp
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        data_url = request.values.get('image')
+        print(data_url)
+        try:
+            os.remove("frameeee.jpg")
+        except:
+            print('nuts')
+        os.system("ffmpeg -i " + path + " -ss " + data_url + " -frames 1 frameeee.jpg")
+@app.route('/draw1', methods = ['GET', 'POST'])
+def draw():
+    if request.method == 'GET':
+        return render_template('index2.html')
+
+
 @app.route(VIDEO_PATH)
 def video():
+    global path
     path = 'videos/demo.mp4'
-    print('nuts')
     start, end = get_range(request)
-    print(start)
-    print(end)
     return partial_response(path, start, end)
+@app.route('/images')
+def image():
+    filename = "frameeee.jpg"
+    return send_file(filename, mimetype='image/gif')
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
